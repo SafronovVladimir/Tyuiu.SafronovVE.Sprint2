95b0f7a4ede86333c31496b0653cc01cb2c7afff
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-13 21:14:29 +0500

    Add project files.

---
+++ Tyuiu.SafronovVE.Sprint2.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.SafronovVE.Sprint2.Task0.V4.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint2.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SafronovVE.Sprint2.Task0.V4.Lib;
+namespace Tyuiu.SafronovVE.Sprint2.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 332;
+            int y = 324;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperation(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
